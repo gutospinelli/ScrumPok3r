8a6602ff5e657da85bedf84a096af1522e6d97ab
Augusto Spinelli <gutospinelli@macbook-air-de-augusto.local>
2016-11-14 17:23:22 -0200

    Seleciona e Desceleciona cartas

--- ScrumPok3r/Pok3rCardsViewController.swift
+++ ScrumPok3r/Pok3rCardsViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class Pok3rCardsViewController: UIViewController {
 
+    var cardSelected = false
+    var buttonSelected: UIButton?
+    
     @IBAction func SelectCard(_ sender: UIButton) {
-        sender.titleLabel?.textColor = UIColor.blue
-        sender.backgroundColor = UIColor.yellow
-        sender.layer.cornerRadius = 5
-        sender.layer.borderWidth = 1
-        sender.layer.borderColor = UIColor.black.cgColor
+        if cardSelected {
+            if buttonSelected != sender {
+                unselectCard(card: buttonSelected!)
+                selectCard(card: sender)
+            }
+        } else {
+            selectCard(card: sender)
+        }
+        
+    }
+    
+    func selectCard(card: UIButton) {
+        card.backgroundColor = #colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1)
+        card.layer.cornerRadius = 5
+        card.layer.borderWidth = 1
+        card.layer.borderColor = UIColor.black.cgColor
+        card.titleLabel?.textColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        cardSelected = true
+        buttonSelected = card
+    }
+    
+    func unselectCard(card: UIButton) {
+        card.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        card.layer.cornerRadius = 0
+        card.layer.borderWidth = 0
+        card.layer.borderColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1).cgColor
+        card.titleLabel?.textColor = #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1)
+        cardSelected = false
+        buttonSelected = nil
     }
 
 
+    @IBOutlet weak var lbSelectedCard: UILabel!
+    
+    
 }
 
