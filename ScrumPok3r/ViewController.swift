828a596c43713cf1b44fb06350ed50df16fda246
Augusto Spinelli <gutospinelli@macbook-air-de-augusto.local>
2016-11-14 16:05:22 -0200

    Initial Commit

---
+++ ScrumPok3r/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrumPok3r
+//
+//  Created by Augusto Spinelli on 14/11/16.
+//  Copyright © 2016 Augusto Spinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
